4c3f03fe9ac717964284054cfae71e20aab4a8b0
양현진 <guswls7081@naver.com>
2018-05-17 21:41:05 +0900

    정보화면 코드추가

--- app/src/main/java/com/example/memes/InformationActivity.java
+++ app/src/main/java/com/example/memes/InformationActivity.java
@@ -1,14 +1,29 @@
 package com.example.memes;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-import com.example.memes.R;
-
-public class InformationActivity extends Activity {
+public class InformationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        ActionBar actionBar = getSupportActionBar(); //액션바 생성
+        actionBar.setDisplayHomeAsUpEnabled(true); //액션바의 뒤로가기 버튼 생성
+    }
+
+    //뒤로가기 버튼 눌렀을 때, 메인화면으로
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
