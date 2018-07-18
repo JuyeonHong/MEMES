83a2c872c8d31af7d461ed46b797250ce0a81b67
양현진 <guswls7081@naver.com>
2018-07-19 01:45:32 +0900

    정보화면에서 액션바의 뒤로가기 버튼을 눌렀을 때 작동하는 함수 수정

--- app/src/main/java/com/example/memes/InformationActivity.java
+++ app/src/main/java/com/example/memes/InformationActivity.java
@@ -20,7 +20,8 @@ public class InformationActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
             case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
+                onBackPressed();
+                //NavUtils.navigateUpFromSameTask(this);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
