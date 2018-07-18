b21641ae62913d916f732ce9a60c86535104872a
bosunny <cbs950886@gmail.com>
2018-07-18 19:11:41 +0900

    widget1.0

--- app/src/main/java/com/example/memes/NewAppWidget.java
+++ app/src/main/java/com/example/memes/NewAppWidget.java
@@ -1,31 +1,45 @@
 package com.example.memes;
 
 import android.app.PendingIntent;
-import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RemoteViews;
+
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+
+
+    public void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.widget_angle, widgetText);
+
+        if(SensorManager.AXIS_Z<20){
+            views.setTextViewText(R.id.appwidget_text, widgetText);
+        }
+        else{
+            views.setTextViewText(R.id.appwidget_textt, widgetText);
+        }
         Intent intent=new Intent(context,MainActivity.class);
 
-        PendingIntent pendingIntent=PendingIntent.getActivity(context,0,intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setOnClickPendingIntent(R.id.widget_angle,pendingIntent);
+        PendingIntent pendingIntent=PendingIntent.getActivity(context,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        views.setOnClickPendingIntent(R.id.appwidget_text,pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -37,21 +51,17 @@ public class NewAppWidget extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
-        Intent intent = new Intent(context, MainTab.UserSensorListner.class);
+        super.onUpdate(context,appWidgetManager,appWidgetIds);
+        Intent intent=new Intent(context,MainTab.UserSensorListener.class);
         context.startService(intent);
-
-
-
     }
+
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
         super.onDeleted(context, appWidgetIds);
-        Intent intent = new Intent(context, MainTab.UserSensorListner.class);
+        Intent intent = new Intent(context,MainTab.UserSensorListener.class);
         context.stopService(intent);
     }
-
-
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
@@ -62,3 +72,4 @@ public class NewAppWidget extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 }
+
