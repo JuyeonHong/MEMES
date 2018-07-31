881761d52320ca15baab634cf53c580843d88ae5
양현진 <guswls7081@naver.com>
2018-08-01 06:47:29 +0900

    Dao에 updateCount, getRecordByDate 추가

--- app/src/main/java/com/example/memes/RangeCountDao.java
+++ app/src/main/java/com/example/memes/RangeCountDao.java
@@ -3,6 +3,7 @@ package com.example.memes;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import java.util.List;
 
@@ -13,4 +14,10 @@ public interface RangeCountDao {
 
     @Query("select * from range_count")
     public List<RangeCount> getAll();
+
+    @Update
+    public void updateCount(RangeCount rangeCount);
+
+    @Query("select * from range_count where date = :date")
+    public RangeCount getRecordByDate(int date);
 }
