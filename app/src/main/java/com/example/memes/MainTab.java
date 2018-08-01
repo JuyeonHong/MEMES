78fbb02bb286a205de055bbbedd64d1f5c91ba74
양현진 <guswls7081@naver.com>
2018-08-02 00:37:50 +0900

    일정 시간이 유지되면 Count를 증가(Database에 직접 count)

--- app/src/main/java/com/example/memes/MainTab.java
+++ app/src/main/java/com/example/memes/MainTab.java
@@ -26,6 +26,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 public class MainTab extends Fragment {
 
     private TextView textViewRealAngle;
@@ -34,8 +38,6 @@ public class MainTab extends Fragment {
     private ImageButton settingsImgBtn;
     private ImageView rotateImg;
     private ImageView circleImg;
-    private TextView countTest1;
-    private TextView countTest2;
 
     /*Used for Accelometer & Gyroscoper*/
     private SensorManager mSensorManager = null;
@@ -151,10 +153,6 @@ public class MainTab extends Fragment {
 
         mToast = Toast.makeText(getActivity(), "null", Toast.LENGTH_SHORT);
 
-        countTest1 = rootView.findViewById(R.id.textViewTemp1);
-        countTest2 = rootView.findViewById(R.id.textViewTemp2);
-
-        //
         ComponentName component = new ComponentName(getContext(), AutoRun.class);
         PackageManager pm = getContext().getPackageManager();
         if(mAppExeMethodindex==0){
@@ -167,7 +165,7 @@ public class MainTab extends Fragment {
                     component,
                     PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
         }
-        //
+
         return rootView;
     }
 
@@ -442,10 +440,6 @@ public class MainTab extends Fragment {
                     oldAngleTime = nowAngleTime;
                 }
             }
-
-
-            countTest1.setText("015: " + count015 + ", 1530: " + count1530 + ", 3045: " + count3045);
-            countTest2.setText("4560: " + count4560 + ", 6090: " + count6090 + ", 90over: " + count90over);
         }
 
         @Override
@@ -454,29 +448,52 @@ public class MainTab extends Fragment {
     }
 
     private void rangeCount(){
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        String getTime = sdf.format(date);
+        int strToInt = Integer.parseInt(getTime);
+
+        RangeCount rc = MainActivity.memesDatabase.rangeCountDao().getRecordByDate(strToInt);
+
         switch (oldAngleRange){
             case ZERO_FIFTEEN:
-                count015++;
+                int zf = rc.getRange0_15();
+                zf = zf + 1;
+                rc.setRange0_15(zf);
                 break;
             case FIFTEEN_THIRTY:
-                count1530++;
+                int ft = rc.getRange15_30();
+                ft = ft + 1;
+                rc.setRange15_30(ft);
                 break;
             case THIRTY_FORTYFIVE:
-                count3045++;
+                int tf = rc.getRange30_45();
+                tf = tf + 1;
+                rc.setRange30_45(tf);
                 break;
             case FORTYFIVE_SIXTY:
-                count4560++;
+                int fs = rc.getRange45_60();
+                fs = fs + 1;
+                rc.setRange45_60(fs);
                 break;
             case SIXTY_NINETY:
-                count6090++;
+                int sn = rc.getRange60_90();
+                sn = sn + 1;
+                rc.setRange60_90(sn);
                 break;
             case NINETY_OVER:
-                count90over++;
+                int no = rc.getRange90over();
+                no = no + 1;
+                rc.setRange90over(no);
                 break;
             default:
                 break;
         }
-        allcount++;
+        int sum = rc.getSumOfAll();
+        sum = sum + 1;
+        rc.setSumOfAll(sum);
+        MainActivity.memesDatabase.rangeCountDao().updateCount(rc);
     }
 
     class Toggle_Activity extends AppCompatActivity {
@@ -505,6 +522,5 @@ public class MainTab extends Fragment {
 
             );
         }
-        
     }
 }
