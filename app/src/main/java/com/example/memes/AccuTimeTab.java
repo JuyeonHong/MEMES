07d11533308d3a31d68730c067ab23c9b41614a8
양현진 <guswls7081@naver.com>
2018-08-03 02:08:42 +0900

    오늘 나의 거북목 상태 그래프

--- app/src/main/java/com/example/memes/AccuTimeTab.java
+++ app/src/main/java/com/example/memes/AccuTimeTab.java
@@ -5,57 +5,118 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import java.util.ArrayList;
+import android.widget.Button;
+
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class AccuTimeTab extends Fragment{
 
-    BarChart chart;
-    ArrayList<BarEntry> BarEntry;
-    ArrayList<String> BarEntryLabels;
-    BarDataSet BarDataSet;
-    BarData BarData;
+    private BarChart todayChart;
+    private BarData barData;
+    private Button btnReset;
+    private Button btnRefresh;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_accutime, container, false);
 
-        chart = rootView.findViewById(R.id.chart1);
-        BarEntry = new ArrayList<>();
-        BarEntryLabels = new ArrayList<String>();
+        todayChart = rootView.findViewById(R.id.todayBarChart);
+        todayChart.animateXY(3000, 3000);
+        todayChart.invalidate();
 
-        AddValuesToBARENTRY();
-        AddValuesToBarEntryLabels();
+        getData();
 
-        BarDataSet = new BarDataSet(BarEntry, "나의 하루 거북목 상태");
-        BarData = new BarData(BarEntryLabels, BarDataSet);
-        BarDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        chart.setData(BarData);
-        chart.animateY(3000);
+        btnReset = rootView.findViewById(R.id.btn_reset);
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int time = nowTime();
+                RangeCount rangeCount;
+                rangeCount = MainActivity.memesDatabase.rangeCountDao().getRecordByDate(time);
+                rangeCount.setRange0_15(0);
+                rangeCount.setRange15_30(0);
+                rangeCount.setRange30_45(0);
+                rangeCount.setRange45_60(0);
+                rangeCount.setRange60_90(0);
+                rangeCount.setRange90over(0);
+                rangeCount.setSumOfAll(0);
+                MainActivity.memesDatabase.rangeCountDao().updateCount(rangeCount);
+                todayChart.animateXY(3000, 3000);
+                todayChart.invalidate();
+
+                getData();
+            }
+        });
+        btnRefresh = rootView.findViewById(R.id.btn_refresh);
+        btnRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                todayChart.animateXY(3000, 3000);
+                todayChart.invalidate();
+                getData();
+            }
+        });
 
         return rootView;
     }
 
-    public void AddValuesToBARENTRY(){
-        BarEntry.add(new BarEntry(2f, 0));
-        BarEntry.add(new BarEntry(4f, 1));
-        BarEntry.add(new BarEntry(6f, 2));
-        BarEntry.add(new BarEntry(8f, 3));
-        BarEntry.add(new BarEntry(7f, 4));
-        BarEntry.add(new BarEntry(3f, 5));
+    private int nowTime() {
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        String getTime = sdf.format(date);
+        int strToInt = Integer.parseInt(getTime);
+
+        return strToInt;
     }
 
-    public void AddValuesToBarEntryLabels(){
-        BarEntryLabels.add("0도~15도");
-        BarEntryLabels.add("15도~30도");
-        BarEntryLabels.add("30도~45도");
-        BarEntryLabels.add("45도~60도");
-        BarEntryLabels.add("60도~90도");
-        BarEntryLabels.add("90도이상");
+    private void getData() {
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("0-15");
+        labels.add("15-30");
+        labels.add("30-45");
+        labels.add("45-60");
+        labels.add("60-90");
+        labels.add("90-");
+
+        int nt = nowTime();
+
+        RangeCount rangeCount = MainActivity.memesDatabase.rangeCountDao().getRecordByDate(nt);
+        int range0_15 = rangeCount.getRange0_15();
+        int range15_30 = rangeCount.getRange15_30();
+        int range30_45 = rangeCount.getRange30_45();
+        int range45_60 = rangeCount.getRange45_60();
+        int range60_90 = rangeCount.getRange60_90();
+        int range90over = rangeCount.getRange90over();
+        int sum = rangeCount.getSumOfAll();
+
+        float p0_15 = (range0_15/(float)sum)*100;
+        float p15_30 = (range15_30/(float)sum)*100;
+        float p30_45 = (range30_45/(float)sum)*100;
+        float p45_60 = (range45_60/(float)sum)*100;
+        float p60_90 = (range60_90/(float)sum)*100;
+        float p90over = (range90over/(float)sum)*100;
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(p0_15, 0));
+        entries.add(new BarEntry(p15_30, 1));
+        entries.add(new BarEntry(p30_45, 2));
+        entries.add(new BarEntry(p45_60, 3));
+        entries.add(new BarEntry(p60_90, 4));
+        entries.add(new BarEntry(p90over, 5));
+
+        BarDataSet dataSet = new BarDataSet(entries, "오늘 거북목 상태");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barData = new BarData(labels, dataSet);
+        todayChart.setData(barData);
     }
 }
