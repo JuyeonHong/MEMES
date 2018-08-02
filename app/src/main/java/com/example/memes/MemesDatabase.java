b855bd255bba7d71fc809022b2b33fd958f33d52
양현진 <guswls7081@naver.com>
2018-08-03 02:07:46 +0900

    스키마 어쩌구 추가

--- app/src/main/java/com/example/memes/MemesDatabase.java
+++ app/src/main/java/com/example/memes/MemesDatabase.java
@@ -3,7 +3,7 @@ package com.example.memes;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(entities = {RangeCount.class}, version = 1)
+@Database(entities = {RangeCount.class}, version = 1, exportSchema = false)
 public abstract class MemesDatabase extends RoomDatabase {
     public abstract RangeCountDao rangeCountDao();
 }
